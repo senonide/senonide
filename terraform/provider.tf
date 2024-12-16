55ccd800331f3f5165e8d923f648cf700d9dd5b8
Sergio Nonide <senonide@gmail.com>
2024-12-16 15:49:42 +0100

    chore: add tf and cicd

---
+++ terraform/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "eu-west-1" # Ireland
+}
