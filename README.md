e8c6e0f18ed744a6244be1135da3d06a99b6f53c
Sergio Nonide <senonide@gmail.com>
2024-12-29 14:44:25 +0100

    chore: upload basic personal website

---
+++ README.md
@@ -0,0 +1,51 @@
+<!--
+**senonide/senonide** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+
+Here are some ideas to get you started:
+
+- 🔭 I’m currently working on ...
+- 🌱 I’m currently learning ...
+- 👯 I’m looking to collaborate on ...
+- 🤔 I’m looking for help with ...
+- 💬 Ask me about ...
+- 📫 How to reach me: ...
+- 😄 Pronouns: ...
+- ⚡ Fun fact: ...
+-->
+![visitors](https://visitor-badge.laobi.icu/badge?page_id=senonide.senonide)&nbsp;
+![version :1.0](https://img.shields.io/badge/version-1.0-informational)&nbsp;
+![profile count](https://komarev.com/ghpvc/?username=senonide&color=red)&nbsp;
+[![GitHub senonide](https://img.shields.io/github/followers/senonide?label=follow&style=social)](https://github.com/senonide)&nbsp;
+![build: passing](https://img.shields.io/badge/build-passing-success)&nbsp;
+
+### Hi there I'm Sergio Nonide 👋
+I am a software engineer specializing in cloud technologies, with a
+particular emphasis on AWS, web development and distributed systems.
+My primary programming language is Go. I am deeply passionate about
+building high-scalability solutions. As a strong proponent of
+open-source software, I actively contribute to and support the
+community. Currently, I am expanding my expertise by learning Rust.
+Additionally, I leverage Linux extensively for both professional and
+personal projects, reflecting my commitment to robust and efficient
+software development practices.
+
+### 🛠 Tech Stack:
+
+![Go](https://img.shields.io/badge/-Go-05122A?style=flat&logo=go)&nbsp;
+![C](https://img.shields.io/badge/-C-05122A?style=flat&logo=C&logoColor=A8B9CC)&nbsp;
+![Rust](https://img.shields.io/badge/-Rust-05122A?style=flat&logo=Rust&logoColor=A8B9CC)
+![Python](https://img.shields.io/badge/-Python-05122A?style=flat&logo=python)&nbsp;
+![JavaScript](https://img.shields.io/badge/-JavaScript-05122A?style=flat&logo=javascript)&nbsp;
+
+
+### 📫 How to reach me:
+<a href="https://www.linkedin.com/in/sergio-nonide-miranda/"><img alt="LinkedIn" src="https://img.shields.io/badge/linkedin%20-%230077B5.svg?&style=flat&logo=linkedin&logoColor=white"/></a> &nbsp;
+
+### 📈 My GitHub Stats:
+<div align="center">
+<br/>
+<img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=senonide&theme=dark&hide_border=false" alt="senonide's Github Stats">
+<br/>
+<br/>
+<img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=senonide&theme=dark&hide_border=false&include_all_commits=true&count_private=true&layout=compact" alt="senonide's languages">
+</div>
---
+++ app/src/App.vue
@@ -0,0 +1,118 @@
+<style scoped>
+header {
+    text-align: center;
+    padding: 50px 0;
+}
+
+h1 {
+    color: #75e6d9;
+    font-size: 2.5rem;
+    margin-bottom: 20px;
+}
+
+p {
+    font-size: 1.1rem;
+    color: #b0b0b0;
+    max-width: 800px;
+    margin: 0 auto;
+}
+
+.tech-stack,
+.contact {
+    text-align: center;
+    margin-top: 30px;
+}
+
+.tech-stack h2,
+.contact h2 {
+    color: #75e6d9;
+    font-size: 2rem;
+}
+
+.tech-stack .icons,
+.contact .icons {
+    margin-top: 20px;
+}
+
+.icons img {
+    width: 50px;
+    margin: 0 10px;
+    margin-bottom: 10px;
+    vertical-align: middle;
+}
+
+footer {
+    text-align: center;
+    margin-top: 50px;
+    color: #aaa;
+}
+
+footer a {
+    color: #75e6d9;
+    text-decoration: none;
+}
+</style>
+
+<template>
+    <header>
+        <h1>Hi there, I'm Sergio</h1>
+        <p>
+            I'm a software engineer specialized in cloud technologies,
+            specifically on AWS, distributed systems and web development. My
+            primary programming language is Go, I love being able to build
+            simple, highly scalable solutions with very good performance with
+            it. Additionally, I am passionate about the world of open source,
+            specifically Linux, which I use on a personal and professional level
+            as my work tool.
+        </p>
+    </header>
+
+    <section class="tech-stack">
+        <h2>🛠 Tech Stack</h2>
+        <div class="icons">
+            <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg"
+                alt="Go Icon"
+            />
+            <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+                alt="Python Icon"
+            />
+            <img
+                src="https://raw.githubusercontent.com/devicons/devicon/refs/tags/v2.16.0/icons/kubernetes/kubernetes-original.svg"
+                alt="K8s Icon"
+            />
+            <img
+                src="https://raw.githubusercontent.com/devicons/devicon/refs/tags/v2.16.0/icons/terraform/terraform-original.svg"
+                alt="Terraform Icon"
+            />
+            <img
+                src="https://raw.githubusercontent.com/devicons/devicon/refs/tags/v2.16.0/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg"
+                alt="Aws Icon"
+            />
+        </div>
+    </section>
+
+    <section class="contact">
+        <h2>Contact me</h2>
+        <div class="icons">
+            <a
+                href="https://www.linkedin.com/in/sergio-nonide-miranda"
+                target="_blank"
+            >
+                <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
+                    alt="LinkedIn Icon"
+                />
+            </a>
+            <!-- Add more icons or links here -->
+        </div>
+    </section>
+
+    <footer>
+        <p>
+            Sergio Nonide -
+            <a href="https://github.com/senonide">GitHub</a>
+        </p>
+    </footer>
+</template>
